39bad953f0ff1ccebcbb61e4521367fdc48b93b0
OprahWinfury <gabrilemugadza@gmail.com>
2016-10-18 21:31:56 +1300

    Fixed bug where the enemy tank wasnt rotating it guns

--- VR-Tank/Assets/Scripts/AI/AI_Rotate.cs
+++ VR-Tank/Assets/Scripts/AI/AI_Rotate.cs
@@ -17,19 +17,17 @@ public class AI_Rotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (target != null)
+        target = FindClosestEnemy();
+        if (Vector3.Distance(target.transform.position, transform.position) < 70)
         {
-            if (Vector3.Distance(transform.position, target.transform.position) < 30)
-            {
-                NextRotation = Quaternion.LookRotation(target.transform.position - transform.position);
-                transform.rotation = Quaternion.Slerp(transform.rotation, NextRotation, Time.time * speed);
-            }
-            else
-            {
-                speed = 0.001f;
-                NextRotation = Quaternion.LookRotation(Parent.forward);
-                transform.rotation = Quaternion.Slerp(transform.rotation, NextRotation, Time.time * speed);
-            }
+            Quaternion looktarget = Quaternion.LookRotation(target.transform.position - transform.position);
+            Quaternion targetHorizontal = transform.rotation;
+
+            targetHorizontal.y = looktarget.y;
+            targetHorizontal.w = looktarget.w;
+
+            transform.rotation = targetHorizontal;
+            Debug.DrawLine(transform.position, transform.parent.forward, Color.blue, 0.1f);
         }
         else
         {
@@ -38,5 +36,25 @@ public class AI_Rotate : MonoBehaviour {
             transform.rotation = Quaternion.Slerp(transform.rotation, NextRotation, Time.time * speed);
         }
 
+    }
+
+    GameObject FindClosestEnemy()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Player");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
